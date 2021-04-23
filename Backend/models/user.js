09d10da900fe29ee3c665dc87779b59226fe1b55
vinita2000 <vinita.yadav@gladiris.com>
2021-04-23 19:10:55 +0530

    added user and teams schema

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,33 +1,46 @@
 const mongoose=require('mongoose');
+// const validator = require('validator');
 
 const userSchema= new mongoose.Schema({
-    name:String,
-    username:String,
-    hash:String,
-    salt:String,
-    verified:{
-        type:Boolean,
-        default:false
+    username: {
+        type: String,
+        unique: true,
+        lowercase: true
     },
-    resetToken:String,
-    expiresIn:Date,
-    verifyOtp:String,
-    otpExpiry:Date,
-    isAdmin:{
-        type:Boolean,
-        default:false
-    },
-    Transactions:[
-        {
-            noOfTokens:Number,
-            fromAccount:String,
-            toAccount:String,
-            gas:String,
-            blockHash:String,
-            transactionHash:String
+    password: {
+        type: String,
+        minlength: 7,
+        validate(value) {
+            // • be a string, 6 chars, atleast one num, atleast one letter
+            if (value.toLowerCase().includes('password') || value.match(/[a-z]/i) && value.match(/[0-9]/)) {
+                throw new Error('Password must contain atleast 6 char, 1 number and 1 letter');
+            }
         }
-    ]
-})
+    },
+    teamChoosen: {
+        type: Boolean,
+        default: false
+    },
+    phoneNumber: {
+        type: Number,
+        validate(value){
+            if (value.length() < 10) {
+                throw new Error('Invalid Phone Number');
+            }
+        }
+    },
+    currCredits: {
+        type: Number,
+        default: 100
+    },
+    totalCredits: {
+        type: Number,
+        default: 100
+    },
+    token: {
+        type: String
+    }
+});
 
 var User=mongoose.model('User',userSchema);
 module.exports=User;
