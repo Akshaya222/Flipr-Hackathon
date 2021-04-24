d86344cc804963c0be7f7cea78c3d06c23bad420
vinita2000 <vinita.yadav@gladiris.com>
2021-04-24 11:54:47 +0530

    added otp send and verification api

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,11 +4,12 @@ const {
   signupUsername,
   signupPhone
 } = require('../controllers/signup');
-
-const {sendOTP} = require('../controllers/user');
-
+const {
+  sendOTP,
+  verifyOTP,
+  resetUserPassword
+} = require('../controllers/user');
 const { auth } = require('../helpers/middlerwares/isLoggedIn');
-
 const {
   loginUsername,
   loginPhone
@@ -18,10 +19,13 @@ router.post('/api/signup-username', signupUsername);
 router.post('/api/signup-phone', signupPhone);
 // router.put('/api/user/login', login);
 
-router.get('/api/send-otp', sendOTP);
-
 // login routes
 router.put('/api/login-username', loginUsername);
 router.put('/api/login-phone', loginPhone);
+
+// private user routes
+router.put('/api/reset-password/:userID', auth, resetUserPassword);
+router.post('/api/send-otp', sendOTP);
+router.post('/api/verify-otp', verifyOTP);
 
 module.exports = router;
